99613a2db9b8d548ecafea271cf2e29e8d8fb1ef
prahlad <prahladkumar7360@gmail.com>
2022-10-17 23:44:35 +0530

    practice api

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,18 +2,17 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
-});
+// router.get('/test-me', function (req, res) {
+//     console.log('My batch is', abc.name)
+//     abc.printName()
+//     res.send('My second ever api!')
+// });
 
-router.get('/students', function (req, res){
-    console.log("The path params in the request are : ", req.params)
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
-
+// router.get('/students', function (req, res){
+//     console.log("The path params in the request are : ", req.params)
+//     let students = ['Sabiha', 'Neha', 'Akash']
+//     res.send(students)
+// })
 
 // Example 1 for path params
 router.get('/students/:studentName', function(req, res){
@@ -39,5 +38,56 @@ router.get('/student-details/:name', function(req, res){
     console.log('Name of the student is ', studentName)
     res.send('Dummy response')
 })
+
+/////////// Probleam No 1
+router.get('/movies',function(req,res){
+    const inter = ["The-Avengers","Doctor Strange","Spider-Man","Black Panther","Thor"]
+   res.send(inter);
+ })
+ 
+ ///////// Probleam No 2
+router.get('/movies/:index', function (req,res){
+   const movie = ["The-Avengers","Doctor Strange","Spider-Man","Black Panther","Thor"]
+   const nameOfmovie =req.params.index
+   res.send(movie[nameOfmovie]);
+})
+ 
+///////// Probleam No 3
+router.get('/Film',function(req,res){
+  const moviearray =  [ { id: 1,
+        name: "The Shining",
+       }, 
+       { id: 2,
+        name: "Incendies",
+       },
+        { id: 3,
+        name: "Rang de Basanti",
+       },
+        { id: 4,
+        name: "Finding Nemo",
+       }]
+       res.send(moviearray);
+})
+
+/////// Probleam No 4
+
+router.get('/Film/:filmid',function(req,res){
+    const array1 =req.params.filmid
+
+    const array =  [ { id: 1,
+          name: "The Shining",
+         }, 
+         { id: 2,
+          name: "Incendies",
+         },
+          { id: 3,
+          name: "Rang de Basanti",
+         },
+          { id: 4,
+          name: "Finding Nemo",
+         }]
+         res.send(array[array1]);
+  })
+
 
 module.exports = router;
